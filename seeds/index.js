e1b9a797814878d13b561ab09902d11fef95a292
attidack <attidack@gmail.com>
2022-06-22 17:52:28 -0600

    added seed file structure

---
+++ seeds/index.js
@@ -0,0 +1,26 @@
+const seedUsers = require('./user-seeds');
+const seedCollections = require('./collection-seeds');
+const seedScrunchies = require('./scrunchies-seeds');
+const seedLogs = require('./log-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------');
+  await seedCollections();
+  console.log('--------------');
+
+  await seedScrunchies();
+  console.log('--------------');
+
+  await seedComments();
+  console.log('--------------');
+
+  await seedLogs();
+  console.log('--------------');
+
+  process.exit(0);
+};
+
+seedAll();
