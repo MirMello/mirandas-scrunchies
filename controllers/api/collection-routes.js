1c3d071516a55e56cd2a4526800cf1c0742e1094
attidack <attidack@gmail.com>
2022-06-24 17:03:30 -0600

    desktop work

--- controllers/api/collection-routes.js
+++ controllers/api/collection-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const {  Collection } = require('../../models');
+const {  Collection, Scrunchie } = require('../../models');
 const withAuth = require('../../utils/auth');
 router.get('/', withAuth, (req, res) => {
     console.log(req.session);
