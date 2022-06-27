c53a2183b087b07ee51d21590cfd925b71495522
attidack <attidack@gmail.com>
2022-06-27 17:17:19 -0600

    added scrunchie add/edit and delete functionality

--- public/javascript/edit-scrunchie.js
+++ public/javascript/edit-scrunchie.js
@@ -1,14 +1,22 @@
 async function editFormHandler(event) {
 event.preventDefault();
 
-const title = document.querySelector('input[name="scrunchie-title"]').value.trim();
-const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-];
+const title = document.querySelector('input[id="scrunchie-title"]').value;
+    const inventory = document.querySelector('input[id="scrunchie-inventory"]').value;
+    const price = parseInt(document.querySelector('input[id="scrunchie-price"]').value);
+    const cogs = document.querySelector('input[id="scrunchie-cost"]').value;
+    const collection_id = document.querySelector('input[id="scrunchie-collection"]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
 const response = await fetch(`/api/scrunchies/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-    title
+    title,
+    inventory,
+    price,
+    cogs,
+    collection_id
     }),
     headers: {
     'Content-Type': 'application/json'
@@ -16,7 +24,7 @@ const response = await fetch(`/api/scrunchies/${id}`, {
 });
 
 if (response.ok) {
-    document.location.replace('/dashboard/');
+    document.location.replace(`/scrunchie/${id}`);
 } else {
     alert(response.statusText);
 }
