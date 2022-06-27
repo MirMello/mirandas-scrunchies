0d4228bd94a27e427eceaff1c6e5aa5170a54321
Cody Eddie <buffbuck0427@gmail.com>
2022-06-26 18:54:48 -0600

    add environmental variables

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
+    host: process.env.HOST,
     dialect: 'mysql',
-    port: 3306
+    port: process.env.PORT
   });
 
 module.exports = sequelize;
